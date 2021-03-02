17e607dbe7b8cb8272c5ba2a13d4ef4aa1a47724
burcu-erkan <burcuerkan.86@gmail.com>
2021-03-02 02:19:09 -0800

    added .js file

---
+++ main.js
@@ -0,0 +1,7 @@
+var child = (3);
+var partner = ("Vahdet");
+var loc = ("Seattle");
+var job = ("Developer");
+
+console.log("You will be a " +job+ " in " + loc + "," +"and married to "+partner + " with "+
+child +" kids" +".");
